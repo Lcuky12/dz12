3d6b8a7cacc2002086ef3c17a89799770436a0f2
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2023-01-12 22:33:36 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int price = 10;
+            int gold;
+            int totalCrystal;
+            Console.WriteLine(" Сколько кристаллов вам нужно? ");
+            totalCrystal = Convert.ToInt32(Console.ReadLine());
+            Console.Write(" Сколько у вас золота? ");
+            gold = Convert.ToInt32(Console.ReadLine());
+            int itog = (gold - price * totalCrystal);
+            Console.WriteLine("У вас осталось золота " + itog);
+            Console.WriteLine("У вас кристаллов " + totalCrystal);
+        }
+    }
+}
